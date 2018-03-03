7cfcf10502039b32c52f38cb02c5f7766ce23c1e
Tomoya Tabuchi <t@tomoyat1.com>
2018-03-03 23:10:40 +0900

    tomoyat1 customizations: tweak mousekey params for ergodox_ez

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -35,9 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COLS 6
 
 #define MOUSEKEY_INTERVAL       20
-#define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    60
-#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_DELAY          40
+#define MOUSEKEY_TIME_TO_MAX    0
+#define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_WHEEL_DELAY 0
 
 #define TAPPING_TOGGLE  1
