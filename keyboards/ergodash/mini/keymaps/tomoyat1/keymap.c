6dfcd4c749e8039c85485983a153c308b3605abb
Tomoya Tabuchi <t@tomoyat1.com>
2019-11-14 02:02:08 +0900

    Add initial ergodash mini layout

--- keyboards/ergodash/mini/keymaps/tomoyat1/keymap.c
+++ keyboards/ergodash/mini/keymaps/tomoyat1/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                              KC_EQL ,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                              KC_RBRC,       KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL ,                              CTL_T(KC_ESC), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL ,                              KC_NO, KC_NO,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
     KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_BSPC,       GUI_T(KC_ENT),KC_LSFT,       RAISE,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
   ),
 
