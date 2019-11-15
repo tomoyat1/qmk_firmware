b322fa66dbb3fcda1d3241b5ba24dd62485e50d5
Tomoya Tabuchi <t@tomoyat1.com>
2019-11-16 00:07:57 +0900

    ergodash mini: minimal viable layout

--- keyboards/ergodash/mini/keymaps/tomoyat1/keymap.c
+++ keyboards/ergodash/mini/keymaps/tomoyat1/keymap.c
@@ -5,13 +5,16 @@ extern keymap_config_t keymap_config;
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
+#define _VIM 3
 #define _ADJUST 16
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
+  VIM,
+  VIM_NRM,
+  GO_ERR,
 };
 
 #define EISU LALT(KC_GRV)
@@ -28,15 +31,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Ctrl |   A  |   S  |   D  |   F  |   G  | Term |                    |  Tab |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  |Delete|                    | Enter|   N  |   M  |   ,  |   .  |   /  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  |Delete|                    |CTLESC|   N  |   M  |   ,  |   .  |   /  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Ctrl |  GUI |  ALt | Lower|||||||| Lower| Space| Bksp |||||||| Bksp | Enter| Raise|||||||| Home |PageDn|PageUp| End  |
+   * | Ctrl |  GUI |  ALt | Lower|||||||| Lower| Space| Bksp |||||||| Bksp |GUIENT| Raise|||||||| Home |PageDn|PageUp| End  |
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                              KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    TERM   ,                              KC_TAB , KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL ,                              CTLESC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL ,                              CTLESC , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TAB , \
     KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_BSPC,              GUIENT,KC_LSFT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
@@ -54,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_EQL , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_UNDS,                        KC_PLUS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TERMN,                          KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12 , \
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_DEL,        KC_BSPC,KC_TRNS, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,                        KC_TRNS, KC_N,    KC_M,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_TRNS, KC_TRNS,       KC_TRNS,KC_TRNS, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Raise
@@ -70,17 +73,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,----------------------------------------------------------------------------------------------------------------------.
   */
   [_RAISE] = LAYOUT(
-    KC_GRV , KC_F1  , KC_F2,   KC_F3  , KC_F4,   KC_F5,   KC_TRNS,                        KC_PLUS, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,  \
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_F12 , \
-    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT, \
-    KC_LCTL, KC_F11 , KC_F12,  EISU,             LOWER,   KC_TRNS, KC_TRNS,       KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_TRNS, KC_EXLM, KC_AT  , KC_LCBR, KC_RCBR, KC_EQUAL,KC_UNDS,                        KC_PLUS, KC_BSLS, KC_7   , KC_8   , KC_9   , KC_F10 , KC_F11 ,  \
+    KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_MINUS,KC_TRNS,                        KC_TRNS, GO_ERR , KC_4   , KC_5   , KC_6   , KC_COLN, KC_F12 , \
+    KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,                        KC_TRNS, KC_AMPR, KC_1   , KC_2   , KC_3   , KC_QUES, KC_TRNS, \
+    KC_TRNS, KC_AMPR, KC_ASTR, KC_TRNS,          LOWER,   KC_TRNS, KC_TRNS,       KC_TRNS,KC_RSFT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
     ),
 
-  /* Adjust
+  /* Vim
    * ,----------------------------------------------------------------------------------------------------------------------.
-   * |      | Reset|RGB ON|  MODE|  HUE-|  HUE+|      |                    |      |  SAT-|  SAT+|  VAL-|  VAL+|      |      |
-   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
+   * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |                    |      | Left | Down |  Up  | Right|      |      |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |      |      |                    |      |      |      |      |      |      |      |
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
@@ -88,8 +91,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_ADJUST] = LAYOUT(
-    _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,_______,                       _______, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, _______, \
-    _______, _______, BL_TOGG, BL_BRTG, BL_INC , BL_DEC ,_______,                       _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, VIM_NRM,_______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,_______,                       _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, \
     _______, _______, _______, _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______  \
   )
@@ -100,28 +103,31 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
     case RAISE:
       if (record->event.pressed) {
         layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       } else {
         layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
       }
       return false;
       break;
-    case ADJUST:
+    case VIM:
       if (record->event.pressed) {
-        layer_on(_ADJUST);
+        layer_on(_VIM);
       } else {
-        layer_off(_ADJUST);
+        layer_off(_VIM);
+      }
+    case VIM_NRM:
+      layer_off(_VIM);
+    case GO_ERR: 
+      if (record->event.pressed) {
+        SEND_STRING("if err != nil {");
+        SEND_STRING(SS_TAP(X_ENTER));
       }
       return false;
       break;
