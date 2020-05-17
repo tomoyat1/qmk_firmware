75b7b34128788d9236261b161a2c7e5f58f11a5a
Tomoya Tabuchi <t@tomoyat1.com>
2020-05-18 07:35:32 +0900

    fix vim layer toggle

--- keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/keymap.c
@@ -1,11 +1,5 @@
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-#define _VIM 3
-#define _ADJUST 16
-
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   VIM_INS,
@@ -21,15 +15,13 @@ enum custom_keycodes {
 #define PASTE LCTL(KC_V)
 #define UNDO LCTL(KC_Z)
 
-#define VIM TG(_VIM)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_60_ansi(
+    [0] = LAYOUT_60_ansi(
         KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL,        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-        CTL_T(KC_GRV),  KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   MO(2),   VIM
+        CTL_T(KC_GRV),  KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   MO(2),   TG(3)
     ),
     [1] = LAYOUT_60_ansi(
         KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
@@ -45,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
     ),
-    [_VIM] = LAYOUT_60_ansi(
+    [3] = LAYOUT_60_ansi(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
         _______, _______, _______, _______, _______, _______, COPY,    UNDO,    VIM_INS, XXXXXXX, PASTE,   XXXXXXX, XXXXXXX, RESET,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,          XXXXXXX,
@@ -57,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case VIM_INS:
-      layer_off(_VIM);
+      layer_off(3);
       return false;
       break;
     case GO_ERR:
