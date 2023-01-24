1749d9610bddd72ab2f08a099c92bc85975583d8
Tomoya Tabuchi <t@tomoyat1.com>
2023-01-24 21:41:27 +0900

    dztech/dz60rgb_ansi/v2: add tap dance to make LCTL share LCMD

--- keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/config.h
+++ keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/config.h
@@ -1,4 +1,4 @@
 #include "config_common.h"
 
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 #define LEADER_TIMEOUT 300
--- keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/keymap.c
+++ keyboards/dztech/dz60rgb_ansi/keymaps/tomoyat1/keymap.c
@@ -7,7 +7,8 @@ enum custom_keycodes {
 };
 
 enum tap_dance_keycodes {
-  TD_ESC
+  TD_ESC, // Unused
+  TD_CTL
 };
 
 #define TERM LGUI(KC_ENT)
@@ -23,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_60_ansi( // default layer
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+        TD(TD_CTL),KC_A,  KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
         LT(5, KC_CLCK), KC_LGUI, KC_LALT,                            KC_SPC,                      KC_RALT, MO(1),   MO(2),   KC_RCTL
     ),
@@ -88,5 +89,6 @@ void td_esc(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_ESC] = ACTION_TAP_DANCE_FN (td_esc)
+  [TD_ESC] = ACTION_TAP_DANCE_FN (td_esc),
+  [TD_CTL] = ACTION_TAP_DANCE_DOUBLE(KC_LCTL, KC_LGUI)
 };
