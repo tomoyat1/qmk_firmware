27ae63ef1d5261fea904ed7d39f668f5e19363ff
Tomoya Tabuchi <t@tomoyat1.com>
2023-09-10 19:02:59 +0900

    ergodash_mini/tomoyat1: add amethyst layer for macOS

--- keyboards/omkbd/ergodash/mini/keymaps/tomoyat1/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/tomoyat1/keymap.c
@@ -6,6 +6,7 @@ extern keymap_config_t keymap_config;
 #define _LOWER 1
 #define _RAISE 2
 #define _VIM 3
+#define _AMETHYST 4
 #define _ADJUST 16
 
 enum custom_keycodes {
@@ -14,6 +15,7 @@ enum custom_keycodes {
   RAISE,
   VIM_INS,
   GO_ERR,
+  AMETHYST,
 };
 
 #define EISU LALT(KC_GRV)
@@ -63,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EQL , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_UNDS,                        KC_PLUS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  \
     KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    TERMN,                          KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12 , \
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,                        KC_TRNS, BL_TOGG, BL_DEC , BL_INC , KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_TRNS, KC_TRNS,       KC_TRNS,KC_TRNS, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_TRNS, KC_TRNS,       KC_TRNS,KC_TRNS, AMETHYST,         KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Raise
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_EXLM, KC_AT  , KC_LCBR, KC_RCBR, KC_EQUAL,KC_UNDS,                        KC_PLUS, KC_BSLS, KC_7   , KC_8   , KC_9   , KC_F10 , KC_F11 ,  \
     KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_MINUS,KC_TRNS,                        KC_TRNS, GO_ERR , KC_4   , KC_5   , KC_6   , KC_COLN, KC_F12 , \
     KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV,  KC_INS ,                        KC_TRNS, KC_AMPR, KC_1   , KC_2   , KC_3   , KC_QUES, KC_TRNS, \
-    KC_TRNS, KC_AMPR, KC_ASTR, KC_TRNS,          LOWER,   KC_TRNS, KC_DEL ,       KC_TRNS,KC_RSFT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_TRNS, KC_AMPR, KC_ASTR, KC_TRNS,          AMETHYST,KC_TRNS, KC_DEL ,       KC_TRNS,KC_RSFT, RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
     ),
 
   /* Vim
@@ -100,7 +102,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,_______,                       _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______, \
     _______, _______, CUT    , _______, _______, _______,_______,                       _______, _______, _______, _______, _______, _______, _______, \
     _______, _______, _______, _______,          _______,_______,_______,       _______,_______, _______,          _______, _______, _______, _______  \
-  )
+  ),
+
+  /* Qwerty
+  * ,----------------------------------------------------------------------------------------------------------------------.
+  * |  `   |   Q  |   W  |   E  |   R  |   T  |   -  |                    |   =  |   Y  |   U  |   I  |   O  |   P  |  \   |
+  * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
+  * | Ctrl |   A  |   S  |   D  |   F  |   G  | Term |                    |  Tab |   H  |   J  |   K  |   L  |   ;  |  "   |
+  * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
+  * | Shift|   Z  |   X  |   C  |   V  |   B  |  Alt |                    |CTLESC|   N  |   M  |   ,  |   .  |   /  | Shift|
+  * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
+  * | Ctrl |  GUI |  ALt | Lower|||||||| Lower| Space| Bksp |||||||| Bksp |GUIENT| Raise|||||||| Home |PageDn|PageUp| End  |
+  * ,----------------------------------------------------------------------------------------------------------------------.
+  */
+
+  [_AMETHYST] = LAYOUT( \
+    KC_SLEP, LSA(KC_1),    LSA(KC_2),    LSA(KC_3),    LSA(KC_4),    LSA(KC_5),    LSA(KC_MINS),                                  KC_EQL,    LSA(KC_6),    LSA(KC_7),    LSA(KC_8),    LSA(KC_9),    LSA(KC_0),    RESET, \
+    KC_TRNS, LSA(KC_A),    LSA(KC_S),    LSA(KC_D),    LSA(KC_F),    LSA(KC_G),    TERM        ,                                  KC_TAB,    LSA(KC_H),    LSA(KC_J),    LSA(KC_K),    LSA(KC_L),    LSA(KC_SCLN), KC_TRNS, \
+    KC_TRNS, LSA(KC_Z),    LSA(KC_X),    LSA(KC_C),    LSA(KC_V),    LSA(KC_B),    LSA(KC_LALT),                                  CTLESC,    LSA(KC_N),    LSA(KC_M),    LSA(KC_COMM), LSA(KC_DOT),  LSA(KC_SLSH), KC_TAB , \
+    KC_TRNS, KC_LGUI,      KC_LALT,      EISU,                       LOWER,        KC_SPC      ,KC_BSPC,          LSA(KC_ENT),LSA(KC_SPC),   RAISE,                      LSA(KC_LEFT), LSA(KC_DOWN), LSA(KC_UP),   LSA(KC_RIGHT)     \
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -118,6 +139,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_RAISE);
       } else {
         layer_off(_RAISE);
+      }
+      return false;
+      break;
+    case AMETHYST:
+      if (record->event.pressed) {
+        layer_on(_AMETHYST);
+      } else {
+        layer_off(_AMETHYST);
       }
       return false;
       break;
